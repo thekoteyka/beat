6a5d9924e9439231383e942c99629a51e3d90393
koteyka5000 <romacat5000@gmail.com>
2023-03-15 23:32:09 +0300

    v2: comments

--- beat.py
+++ beat.py
@@ -7,72 +7,72 @@ chase = 0
 # last_hp_2nd = 100
 class Person:
     def __init__(self, NAME:str):
-        self.NAME:str = NAME
-        self.hp:int = 100
-        self.DAMAGE:int = 10
+        self.NAME:str = NAME   # Имя персонажа
+        self.hp:int =     100  # Здоровье в начале игры
+        self.DAMAGE:int = 10   # Стандартный урон
 
-        self.CRIT_RATE:float = 0.4
-        self.CRIT_DMG:int = 20
-        self.CRIT_HEAL = 30
+        self.CRIT_RATE:float = 0.4 # Шанс критического попадания
+        self.CRIT_DMG:int =    20  # Урон при критическом попадании
+        self.CRIT_HEAL =       30  # Лечение при нанесении критического урона
 
-        self.VAMPIRE_RATE:float = 0.2
-        self.VAMPIRE_HP_STEAL:int = 10
+        self.VAMPIRE_RATE:float =   0.2 # Шанс на кражу здоровья у дрогого игрока
+        self.VAMPIRE_HP_STEAL:int = 10  # Количество украденого здоровья 
         
-        self.alive:bool = True
+        self.alive:bool = True  # Жив ли игрок
         
-        self.totem_of_immortality:int = 1
+        self.totem_of_immortality:int = 1  # Количество Тотемов Бессметрия (Позволяет возродиться 1 раз)
 
-    def is_crit(self) -> bool:
+    def is_crit(self) -> bool:  # Кританул ли
         if random() <= self.CRIT_RATE:
             return True
         return False
     
-    def is_vampirize(self) -> bool:
+    def is_vampirize(self) -> bool:  # Украл здоровье ли
         if random() <= self.VAMPIRE_RATE:
             return True
         return False
     
-    def printuwu(self, s, other):
-        s = str(s)
-        max_spaces = 28 + len(self.NAME) + len(other.NAME) + 5
-        spaces = (max_spaces - len(s)) * ' '
+    def printuwu(self, s, other):  # Особый вывод с показом здоровья справа
+        s = str(s) # На всякий случай
+        max_spaces = 28 + len(self.NAME) + len(other.NAME) + 5 # Максимальная длинна строки
+        spaces = (max_spaces - len(s)) * ' ' # Сколько осталось пробелов до макс длинны
 
-        hp1st = self.hp
-        hp2nd = other.hp
+        hp1st = self.hp  # Хп первого игрока
+        hp2nd = other.hp # Хп второго игрока
 
-        if chase == 0:
+        if chase == 0: # Чей ход
             spaces_between_hp = (3 - len(str(hp1st))) * ' '
             print(f'{s}{spaces} | {Fore.LIGHTRED_EX} {hp1st}{spaces_between_hp} | {hp2nd}')
         else:
             spaces_between_hp = (3 - len(str(hp2nd))) * ' '
             print(f'{s}{spaces} | {Fore.LIGHTRED_EX} {hp2nd}{spaces_between_hp} | {hp1st}')
 
-    def die(self):
-        if self.totem_of_immortality:
+    def die(self):  # Попробовать умереть
+        if self.totem_of_immortality: # Если есть тоттем
             self.hp = 100
-            self.totem_of_immortality -= 1
+            self.totem_of_immortality -= 1 # Забираем один тоттем
             print(f'{Fore.YELLOW}{self.NAME} использовал Тотем Бессмертия! Осталось: {self.totem_of_immortality}')
             return
-        self.alive = False
+        self.alive = False # Иначе персонаж погибает
 
-    def damage_it(self, amount:int):
+    def damage_it(self, amount:int): # Нанести урон
         self.hp -= amount
-        if self.hp < 0:
+        if self.hp < 0: # Чтобы не было отрицательного хп
             self.hp = 0
     
-    def __add__(self, other):
+    def __add__(self, other): # персонаж (атакующий) + персонаж
         if self.is_vampirize():
             other.damage_it(self.VAMPIRE_HP_STEAL)
             self.hp += self.VAMPIRE_HP_STEAL
             self.printuwu(f'{Fore.CYAN}{self.NAME} украл {self.VAMPIRE_HP_STEAL} здоровья у {other.NAME}', other)
         
-        if self.is_crit():
+        if self.is_crit(): # Если кританул
             damage = self.CRIT_DMG
             other.damage_it(damage)
             self.printuwu(f'{Fore.LIGHTGREEN_EX}{self.NAME} нанёс {damage} крит урона игроку {other.NAME}', other)
             self.hp += self.CRIT_HEAL
             self.printuwu(f'{Fore.BLUE}{self.NAME} восстановил {self.CRIT_HEAL} хп', other)
-        else:
+        else: # Если не кританул
             damage = self.DAMAGE
             other.damage_it(damage)
             self.printuwu(f'{Fore.LIGHTWHITE_EX}{self.NAME} нанёс {damage} урона игроку {other.NAME}', other)
@@ -80,7 +80,7 @@ class Person:
         if other.hp <= 0:
             other.die()
         
-        if not other.alive:
+        if not other.alive: # Если противник не жив (то есть мёртв)
             self.printuwu(f"{Fore.YELLOW}{self.NAME} убил игрока {other.NAME}!", other)
             return 'finish'
 
@@ -95,7 +95,7 @@ while not result == 'finish':
     else:
         result = nikita + me
     print()
-    chase = 1 if chase == 0 else 0
-    sleep(0.1)
+    chase = 1 if chase == 0 else 0 # Смена хода
+    sleep(2)
 
     
