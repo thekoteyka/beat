a8d12e9fd9c92035fbea64aadccd25292a3ba3ff
koteyka5000 <romacat5000@gmail.com>
2023-03-15 22:55:19 +0300

    v1 full

--- beat.py
+++ beat.py
@@ -1,7 +1,8 @@
 from random import random
 from colorama import init, Fore
+from time import sleep
 init(autoreset=True)
-
+chase = 0
 class Person:
     def __init__(self, NAME:str):
         self.NAME:str = NAME
@@ -28,36 +29,55 @@ class Person:
         return False
     
     def printuwu(self, s, other):
-        spaces = (41 - len(s)) * ' '
+        s = str(s)
+        max_spaces = 28 + len(self.NAME) + len(other.NAME) + 5
+        spaces = (max_spaces - len(s)) * ' '
         hp1st = self.hp
         hp2nd = other.hp
-        print(f'{s}{spaces} | {Fore.LIGHTRED_EX} {hp1st} | {hp2nd}')
+        if chase == 0:
+            print(f'{s}{spaces} | {Fore.LIGHTRED_EX} {hp1st} | {hp2nd}')
+        else:
+            print(f'{s}{spaces} | {Fore.LIGHTRED_EX} {hp2nd} | {hp1st}')
 
     def __add__(self, other):
-        if not other.alive:
-            return 'finish'
-
         if self.is_vampirize():
             other.hp -= self.VAMPIRE_HP_STEAL
             self.hp += self.VAMPIRE_HP_STEAL
-            print(f'{Fore.CYAN}{self.NAME} украл {self.VAMPIRE_HP_STEAL} здоровья у {other.NAME} |{Fore.LIGHTRED_EX} {self.hp} | {other.hp}')
+            self.printuwu(f'{Fore.CYAN}{self.NAME} украл {self.VAMPIRE_HP_STEAL} здоровья у {other.NAME}', other)
         
         if self.is_crit():
             damage = self.CRIT_DMG
             other.hp -= damage
-            print(f'{Fore.LIGHTGREEN_EX}{self.NAME} нанёс {damage} крит урона игроку {other.NAME} |{Fore.LIGHTRED_EX} {self.hp} | {other.hp}')
+            self.printuwu(f'{Fore.LIGHTGREEN_EX}{self.NAME} нанёс {damage} крит урона игроку {other.NAME}', other)
             self.hp += self.CRIT_HEAL
-            print(f'{Fore.BLUE}{self.NAME} восстановил {self.CRIT_HEAL} хп')
+            self.printuwu(f'{Fore.BLUE}{self.NAME} восстановил {self.CRIT_HEAL} хп', other)
         else:
             damage = self.DAMAGE
             other.hp -= damage
-            print(f'{self.NAME} нанёс {damage} урона игроку {other.NAME} |{Fore.LIGHTRED_EX} {self.hp} | {other.hp}')
+            self.printuwu(f'{Fore.LIGHTWHITE_EX}{self.NAME} нанёс {damage} урона игроку {other.NAME}', other)
         
+        if other.hp <= 0:
+            other.alive = False
+            other.hp = 0
         
         if not other.alive:
-            print(f"{Fore.YELLOW}{self.NAME} убил игрока {other.NAME}! |{Fore.LIGHTRED_EX} {self.hp}")
+            self.printuwu(f"{Fore.YELLOW}{self.NAME} убил игрока {other.NAME}!", other)
+
+        if not other.alive:
+            return 'finish'
 
         
-me = Person('Котейка')
-nikita = Person('Никита')
-me + nikita
+me = Person('Котейка')    # 1st
+nikita = Person('Никита') # 2nd
+result = None
+
+while not result == 'finish':
+    if chase == 0:
+        result = me + nikita
+    else:
+        result = nikita + me
+    print()
+    chase = 1 if chase == 0 else 0
+    sleep(1)
+
+    
